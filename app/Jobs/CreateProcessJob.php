1f44ab7304256f54a5eff2af69c3b2dde1356ca9
Rafael Teixeira <rafaeldemeirateixeira@gmail.com>
2021-01-17 12:41:12 -0300

    [Feature] Added job to create process log

---
+++ app/Jobs/CreateProcessJob.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class CreateProcessJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * @var Order
+     */
+    private Order $order;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->order->process()->create([
+            'job_uuid' => $this->job->uuid(),
+            'attempt' => $this->attempts(),
+            'message' => 'started'
+        ]);
+    }
+}
